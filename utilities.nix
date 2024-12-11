4baf133780f7a037ffeedacb8a9dff1396986ce2
locmai <locmai0201@gmail.com>
2024-12-11 15:04:33 +0700

    bye goku

--- utilities.nix
+++ utilities.nix
@@ -5,4 +5,21 @@
   environment.systemPackages = with pkgs; [
     karabiner-elements
   ];
+
+  nixpkgs.overlays = [
+    (self: super: {
+      karabiner-elements = super.karabiner-elements.overrideAttrs (old: {
+        version = "14.13.0";
+
+        src = super.fetchurl {
+          inherit (old.src) url;
+          hash = "sha256-gmJwoht/Tfm5qMecmq1N6PSAIfWOqsvuHU8VDJY8bLw=";
+        };
+      });
+    })
+  ];
+
+  services = {
+    karabiner-elements.enable = true;
+  };
 }
